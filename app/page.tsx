899b8b4849701f7dc2bc1fc97bc249b2db9ebd85
guilherme-donato-dev <donatocruzg@gmail.com>
2025-03-02 16:49:35 -0300

    alterado para 3 projetos

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ const projects = [
   { name: "Sistema de revenda de carros.", description: "Um sistema de revenda de carros online, feito totalmente com Django e usando o PostgreSQL como Banco de dados.", link: "http://3.217.69.134/cars/" },
   { name: "Chatbot com RAG utilizando a API da OpenAI.", description: "Um chatbot usando a API da OpenAI. O chat pode fazer RAG, subindo um ou mais documentos. Pode também escolher qual modelo da OpenAI será utilizado.", link: "#" },
   { name: "Projeto 3", description: "Descrição do Projeto 3", link: "#" },
-  { name: "Projeto 4", description: "Descrição do Projeto 4", link: "#" },
-  { name: "Projeto 5", description: "Descrição do Projeto 5", link: "#" },
 ];
 
 export default function Home() {
@@ -20,7 +18,7 @@ export default function Home() {
           <a
             key={index}
             href={project.link}
-            className="block bg-black p-6 rounded-lg shadow-xl border border-white hover:shadow-2xl transition transform hover:scale-105 hover:bg-blue-900/20"
+            className="block bg-black p-6 rounded-lg shadow-xl border border-white hover:shadow-2xl transition transform hover:scale-105"
           >
             <h2 className="text-xl font-semibold mb-2">{project.name}</h2>
             <p className="text-gray-400">{project.description}</p>
