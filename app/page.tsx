eebd9e84fa53b115ac2212273c363e2d95d28b9d
guilherme-donato-dev <donatocruzg@gmail.com>
2025-03-23 09:57:23 -0300

    deployar

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,13 @@ import "@fontsource/slabo-27px";
 const projects = [
   { name: "Sistema de revenda de carros.", description: "Um sistema de revenda de carros online, feito totalmente com Django e usando o PostgreSQL como Banco de dados.", link: "http://3.217.69.134/cars/" },
   { name: "Chatbot com RAG utilizando a API da OpenAI.", description: "Um chatbot usando a API da OpenAI. O chat pode fazer RAG, subindo um ou mais documentos. Pode também escolher qual modelo da OpenAI será utilizado.", link: "https://chatbot-rag-production-5f7e.up.railway.app/" },
-<<<<<<< HEAD
-  { name: "Projeto 3", description: "Descrição do Projeto 3", link: "#" },
-=======
-  { name: "Biblioteca de Filmes - API RESTful", description: "API RESTful para uma biblioteca de filmes com Django Rest Framework, autenticação JWT e permissões para usuários cadastrados. Frontend em Streamlit exibindo estatísticas, filmes, gêneros, atores e permitindo avaliações.", link: "#" },
->>>>>>> e12b55a (title guilhermedonato.dev)
+  { name: "Biblioteca de Filmes - API RESTful ", description: "API RESTful para uma biblioteca de filmes com Django Rest Framework, autenticação JWT e permissões para usuários cadastrados. Frontend em Streamlit exibindo estatísticas, filmes, gêneros, atores e permitindo avaliações.", link: "#" },
 ];
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>guilhermedonato.dev</title>
-      </Head>
+     
       <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white font-serif p-4">
         <h1 className="text-3xl font-bold mb-3">guilhermedonato.dev</h1>
         <p className="text-base mb-4">Desenvolvedor Backend | Python</p>
